a1c8088355a81f755dcf867ee7f339f9d3112d5d
wat <wat>
2021-05-10 10:51:29 +0900

    20210510 Laravel Mix Install

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('test');
-});
+// Route::get('/', function () {
+//     return view('test');
+// });
+
+Route::get('/{any}', function() {
+    return view('app');
+})->where('any', '.*');
